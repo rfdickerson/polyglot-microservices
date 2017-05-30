f2bbd8550212cf8508485e22ab0044430771aa5f
Robert F. Dickerson <rfdickerson@us.ibm.com>
2017-05-30 18:19:13 -0500

    Add Travis

--- python-microservice/app.py
+++ python-microservice/app.py
@@ -1,9 +1,16 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
+try:
+    developMode = os.environ['PYTHON_ENV'] == 'development'
+except KeyError:
+    print "No environment mode set, defaulting to production"
+    developMode = False
+
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello,  World!"
 
 if __name__ == "__main__":
-    app.run(debug=True,host='0.0.0.0')
+    app.run(debug=developMode,host='0.0.0.0')
